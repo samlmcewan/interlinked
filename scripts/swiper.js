7543749d65d6801e94b27d1f04c771126828e229
samlmcewan <samlmcewan@gmail.com>
2024-08-07 22:47:15 +0100

    finish accreditations

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,12 +1,30 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  // direction: 'horizontal',
+  direction: 'horizontal',
   loop: true,
   autoplay: {
     delay: 5000, 
     disableOnInteraction: false,
   },
-  // spaceBetween: -5
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+      
+    },
+    992: {
+      slidesPerView: 4
+      
+    },
+    1200: {
+      slidesPerView: 5
+    }
+  }
 });
 
 
